7261dd5f152e30cd43eebaaa30031faefe344330
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-01 17:05:16 +0100

    Demo container

---
+++ demo_container.R
@@ -0,0 +1,19 @@
+library(ormr)
+
+ormr_report(ormr_folder_name = "/opt/ormr")
+
+python_script_path <- system.file("extdata", "scipy_example.py", package = "ormr")
+
+ormr::run_python_script(
+  ormr_folder_name = "/opt/ormr",
+  python_script_path = python_script_path
+)
+
+if (1 == 2) {
+  python_script_path <- "scipy_example.py"
+  download.file(
+    url = "https://raw.githubusercontent.com/richelbilderbeek/ormr/master/inst/extdata/scipy_example.py",
+    destfile = python_script_path
+  )
+  reticulate::py_run_file(file = python_script_path)
+}
